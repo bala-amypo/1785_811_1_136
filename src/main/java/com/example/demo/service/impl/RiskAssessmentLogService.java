867a2783b8a2ff95ddd1e5d9757383a5d6d8391e
bala-amypo <bala@amypo.in>
2025-12-22 06:34:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RiskAssessmentLogService.java
+++ src/main/java/com/example/demo/service/impl/RiskAssessmentLogService.java
@@ -1,11 +1,11 @@
 package com.example.demo.service.impl;
+
 import org.springframework.stereotype.Service;
+
 @Service
-public class RiskAssessmentLogService
-{
-    public String assessRisk()
-    {
+public class RiskAssessmentLogService {
+
+    public String assessRisk() {
         return "Risk Assessed";
     }
 }
-​
