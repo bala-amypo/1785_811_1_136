dcdedf8cea6db5cdd32c693a0dab4e4bf33026a4
bala-amypo <bala@amypo.in>
2025-12-22 03:45:53 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,14 @@
+controller/AuthController.java
+
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/auth")
+public class AuthController
+{
+@PostMapping("/login")
+public String login()
+    {
+    return "Login Successful";
+    }
+}
---
+++ src/main/java/com/example/demo/controller/EligibilityController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("/eligibility")
+public class EligibilityController
+{
+@GetMapping("/{userId}")
+public String checkEligibility(@PathVariable Long userId)
+{
+return "Eligible";
+}
+}
