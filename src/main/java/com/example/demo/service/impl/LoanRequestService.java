4f05251db87ee67fb66140a65f1b0a7577eb65eb
bala-amypo <bala@amypo.in>
2025-12-22 04:32:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/LoanRequestService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service.impl;
+import org.springframework.stereotype.Service;
+@Service
+public class LoanRequestService
+{
+    public String requestLoan()
+        {
+        return "Loan Requested";
+        }
+}
