ad6bc3028280ff97649aa1127f61e433b2bff638
bala-amypo <bala@amypo.in>
2025-12-26 23:54:13 +0000

    User commit

--- src/main/java/com/example/demo/dto/LoanDtos.java
+++ src/main/java/com/example/demo/dto/LoanDtos.java
@@ -1,1 +1,41 @@
-v
+vpackage com.example.demo.dto;
+
+public class LoanDtos {
+
+    private Long userId;
+    private Double amount;
+    private Integer tenure;
+    private Double interestRate;
+
+    public Long getUserId() {
+        return userId;
+    }
+    
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+    
+    public Double getAmount() {
+        return amount;
+    }
+    
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+    
+    public Integer getTenure() {
+        return tenure;
+    }
+    
+    public void setTenure(Integer tenure) {
+        this.tenure = tenure;
+    }
+    
+    public Double getInterestRate() {
+        return interestRate;
+    }
+    
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
