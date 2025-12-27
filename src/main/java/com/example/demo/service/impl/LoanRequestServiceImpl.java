7dedde0e49caa7f0fbe0c829881ddc9c279902ba
bala-amypo <bala@amypo.in>
2025-12-27 03:36:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/LoanRequestServiceImpl.java
@@ -2,34 +2,28 @@ package com.example.demo.service.impl;
 
 import com.example.demo.entity.LoanRequest;
 import com.example.demo.repository.LoanRequestRepository;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.service.LoanRequestService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class LoanRequestServiceImpl {
+public class LoanRequestServiceImpl implements LoanRequestService {
 
-    public LoanRequestServiceImpl() {
+    private final LoanRequestRepository loanRequestRepository;
+
+    public LoanRequestServiceImpl(LoanRequestRepository loanRequestRepository) {
+        this.loanRequestRepository = loanRequestRepository;
     }
 
-    public LoanRequestServiceImpl(
-            LoanRequestRepository repo,
-            UserRepository userRepo
-    ) {
+    @Override
+    public LoanRequest create(LoanRequest loanRequest) {
+        loanRequest.setStatus(LoanRequest.Status.PENDING.name());
+        return loanRequestRepository.save(loanRequest);
     }
 
-    public LoanRequest submitRequest(LoanRequest lr) {
-        lr.setStatus(LoanRequest.Status.PENDING);
-        return lr;
-    }
-
-    public List<LoanRequest> getRequestsByUser(long userId) {
-        return new ArrayList<>();
-    }
-
-    public LoanRequest getById(long id) {
-        return new LoanRequest();
+    @Override
+    public List<LoanRequest> getByUserId(Long userId) {
+        return loanRequestRepository.findByUserId(userId);
     }
 }
