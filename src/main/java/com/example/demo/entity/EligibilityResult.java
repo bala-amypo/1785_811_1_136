e1dd32a411618ae9c1ed48218353b3d290465b4b
bala-amypo <bala@amypo.in>
2025-12-27 02:10:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/EligibilityResult.java
+++ src/main/java/com/example/demo/entity/EligibilityResult.java
@@ -3,36 +3,39 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
-public class EligibilityResult {
-
+public class EligibilityResult
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "loan_request_id")
     private Long loanRequestId;
+
     private Double maxEligibleAmount;
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getLoanRequestId() {
+    public Long getLoanRequestId()
+    {
         return loanRequestId;
     }
 
-    public void setLoanRequestId(Long loanRequestId) {
+    public void setLoanRequestId(Long loanRequestId)
+    {
         this.loanRequestId = loanRequestId;
     }
 
-    public Double getMaxEligibleAmount() {
+    public Double getMaxEligibleAmount()
+    {
         return maxEligibleAmount;
     }
 
-    public void setMaxEligibleAmount(Double maxEligibleAmount) {
+    public void setMaxEligibleAmount(Double maxEligibleAmount)
+    {
         this.maxEligibleAmount = maxEligibleAmount;
     }
 }
