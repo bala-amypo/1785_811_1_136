ee8d463bfb5c33edd7f0c23ecbe6e687b37c80f2
bala-amypo <bala@amypo.in>
2025-12-27 00:34:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -3,40 +3,64 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "products")
-public class Product {
-
+public class Product
+{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String name;
-
-    @Column(nullable = false)
     private double interestRate;
+    private int maxTenureMonths;
+    private double maxAmount;
 
-    public Long getId() {
+    public Long getId()
+    {
         return id;
     }
-    
-    public void setId(Long id) {
+
+    public void setId(Long id)
+    {
         this.id = id;
     }
-    
-    public String getName() {
+
+    public String getName()
+    {
         return name;
     }
-    
-    public void setName(String name) {
+
+    public void setName(String name)
+    {
         this.name = name;
     }
-    
-    public double getInterestRate() {
+
+    public double getInterestRate()
+    {
         return interestRate;
     }
-    
-    public void setInterestRate(double interestRate) {
+
+    public void setInterestRate(double interestRate)
+    {
         this.interestRate = interestRate;
+    }
+
+    public int getMaxTenureMonths()
+    {
+        return maxTenureMonths;
+    }
+
+    public void setMaxTenureMonths(int maxTenureMonths)
+    {
+        this.maxTenureMonths = maxTenureMonths;
+    }
+
+    public double getMaxAmount()
+    {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(double maxAmount)
+    {
+        this.maxAmount = maxAmount;
     }
 }
