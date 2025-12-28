03c329fb1f723b6041a79b274b76a5afc09798c7
bala-amypo <bala@amypo.in>
2025-12-28 14:09:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -8,8 +8,11 @@ import com.example.demo.security.JwtUtil;
 import com.example.demo.service.impl.UserServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
+@RestController
+@RequestMapping("/auth")
 public class AuthController
 {
     private final UserServiceImpl userService;
@@ -24,7 +27,8 @@ public class AuthController
         this.userRepository = userRepository;
     }
 
-    public ResponseEntity<AuthResponse> login(AuthRequest request)
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request)
     {
         User user = userRepository.findByEmail(request.getEmail()).orElseThrow();
 
